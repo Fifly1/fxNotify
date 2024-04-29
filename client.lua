0d3090893f95a68f0e633f4be3ff5554993dcff4
fifly <107129715+Fifly1@users.noreply.github.com>
2024-04-29 15:42:27 -0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -13,11 +13,4 @@ RegisterNetEvent("fx_notify:Notify")
     fx_notify(type, title, message, time)
 end)
 
-RegisterCommand("notify-test", function()
-    exports['fx_notify']:fx_notify("info", "Police", "A criminal is on the run!", 5000)	
-    exports['fx_notify']:fx_notify("success", "Police", "A criminal is on the run!", 5000)	
-    exports['fx_notify']:fx_notify("warning", "Police", "A criminal is on the run!", 5000)	
-    exports['fx_notify']:fx_notify("error", "Police", "A criminal is on the run!", 5000)	
-end)
-
 exports['fx_notify']:fx_notify("success", "title title", "text text text", 2000)	
